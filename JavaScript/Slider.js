3bc8b4a1d3bb7207e887bf7903f5c7bd7e1beba0
hamid habibi <hamidhabibiaa@gmail.com>
2021-08-16 12:25:44 +0430

    this project is done

--- JavaScript/Slider.js
+++ JavaScript/Slider.js
@@ -1,56 +1,112 @@
-// this part is for dinamic js 
-// three parts need change:
-// 1:search button  2:items  3:add button
+// trying to get the value of local storage
 
+initialValue = localStorage.getItem("initialValue");
 
-// 1:search button => changing the shape of button on first click
-// after filling the input on key press search will began
-const searchBtn = document.querySelector(".serach-btn");
-const searchBox = searchBtn.previousElementSibling;
-let items = document.querySelectorAll(".item");
+try {
+    initialValue = JSON.parse(initialValue)
+    initialValue = initialValue.length ? initialValue : null ;
+} catch (e) {
+    initialValue = null
+}
+if (initialValue == null) {
+  initialValue = [
+      {content : "option 1" , status : true}
+  ]
+}
+localStorage.setItem("initialValue", JSON.stringify(initialValue))
+initialValue = JSON.parse(localStorage.getItem("initialValue"));
+
+// func that controls local storage
+function controller(initialValue) {
+    let list = document.querySelector(".list-items")
+    list.innerHTML = ""
+    Array.from(initialValue).forEach((value, index) => {
+        // creating an li and its chils 
+        let li = document.createElement("li");
+        let text = document.createElement("span");
+        let deleteBtn = document.createElement("span");
+        li.className = "item";
+        text.className = "text-content";
+        text.textContent = value.content;
+        text.style.textDecoration = value.status ? "initial" : "line-through"
+        deleteBtn.className = "zmdi zmdi-delete";
+        // adding childs to li
+        li.append(text);
+        li.append(deleteBtn);
+        // adding li in to document
+        list.append(li);
+
+        // removal button functions
+        deleteBtn.addEventListener("click", e => {
+            initialValue.splice(index , 1);
+            localStorage.setItem("initialValue" , JSON.stringify(initialValue))
+            controller(initialValue);
+        })
+        // seeing if th content is done
+        text.addEventListener("click" , e => {
+            value.status = !value.status;
+            localStorage.setItem("initialValue" , JSON.stringify(initialValue))
+            controller(initialValue);
+        })
+
+        // adding new item to todos 
+        const addBtn =document.querySelector("button.add-btn");
+        addBtn.addEventListener("click" , e => {
+            e.preventDefault();
+            const parent = addBtn.parentElement;
+            let addValue = addBtn.previousElementSibling;
+            const addCloser = addValue.previousElementSibling;
+            if (addBtn.innerText == "add") {
+               addBtn.innerText ="create";
+               parent.classList.add("clicked");
+            }else{
+                addBtn.innerText = "add";
+               parent.classList.remove("clicked");
+               if (addValue) {
+                   addValue = {content : addValue.value , status : true};
+                   initialValue.push(addValue);
+                   localStorage.setItem( "initialValue" , JSON.stringify(initialValue));
+                  
+                   controller(initialValue);
+                   addBtn.previousElementSibling.value = null
+               }
+
+            }
+            addCloser.addEventListener("click" , () => {
+                addBtn.innerText = "add";
+                parent.classList.remove("clicked");
+                addBtn.previousElementSibling.value = null
+            })
+        })
+
+    });
+}
+controller(initialValue);
+
+// adding search options to project
+const searchBtn = document.querySelector(".search-btn");
 searchBtn.addEventListener("click" , () => {
-    const searchWrapper = searchBtn.parentElement;
-    searchWrapper.classList.toggle("active");
-    searchBox.value = "";
-    if (searchWrapper.classList[1]) {
-        searchBtn.innerHTML = "<li class='zmdi zmdi-close'></li>"
+   let searchBox = searchBtn.parentElement;
+   let searchValue = searchBtn.previousElementSibling;
+    if (searchBtn.innerHTML == "search") {
+        searchBtn.innerHTML ="<li class='zmdi zmdi-close'></li>";
+        searchBox.classList.add("active")
     } else {
-        searchBtn.innerHTML = "search"  
+        searchBtn.innerHTML = "search";
+        searchBox.classList.remove("active");
+        searchValue.value = null;
     }
-    items.forEach(item => {
-        item.style.display = "flex"
+    searchValue.addEventListener("keyup" , e => {
+       
+        document.querySelectorAll(".item").forEach(item => {
+            
+            if(item.innerText.includes(searchValue.value))
+            {
+                item.style.display = "flex"
+            }else{
+                item.style.display = "none"
 
+            }
+        })
     })
-
-})
-searchBox.addEventListener("keyup" , () => {
-    const searchValue = searchBox.value;
-    console.log(searchValue);
-    items.forEach(item => {
-        let itemValue = item.textContent;
-      if (itemValue.includes(searchValue)) {
-        item.style.display = "flex"
-          
-      } else {
-          item.style.display = "none"
-      }
-   
-     
-    })
-    
-})
-const addBtn = document.querySelector(".add-btn");
-addBtn.addEventListener("click" , () => {
-    const addBox = addBtn.previousElementSibling;
-    if (addBtn.innerHTML == "add") {
-        addBtn.innerHTML = "create";
-        addBox.style.display = "flex";
-    } else {
-        const newItemValue = addBox.value;
-        const newItem = items[0].cloneNode(true);
-        addBtn.innerHTML = "add";
-        addBox.style.display = "none";
-        addBox.value= null;
-        
-    }
 })
